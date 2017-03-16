60c0fdaf43a469d37a6bfaba784261e5bc064bad
ChristianPisani <christian96-th@hotmail.com>
2017-03-16 14:06:32 +0100

    SOS page

--- Fadderullan_BI_Bergen/EventsPage.xaml.cs
+++ Fadderullan_BI_Bergen/EventsPage.xaml.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
-using Plugin.Messaging;
-using Plugin.Geolocator;
-
 namespace Fadderullan_BI_Bergen {
 	public partial class EventsPage : ContentPage {
 		ObservableCollection<Event> events = new ObservableCollection<Event>();
@@ -30,14 +27,6 @@ namespace Fadderullan_BI_Bergen {
 		}
 
 		async void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e) {
-			var SmsTask = MessagingPlugin.SmsMessenger;
-
-			var position = await CrossGeolocator.Current.GetPositionAsync(timeoutMilliseconds: 10000);
-			String pos = "Fadderullan SOS knapp test \nhttp://maps.google.com/?q=" + position.Latitude + "," + position.Longitude;
-
-			if (SmsTask.CanSendSms)
-				SmsTask.SendSms("+4790950497", pos);
-
 			Event ev = (Event)Events.SelectedItem;
 			String detail = ev.description;
 
---
+++ Fadderullan_BI_Bergen/SOSPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Plugin.Geolocator;
+using Plugin.Messaging;
+
+using Xamarin.Forms;
+
+namespace Fadderullan_BI_Bergen {
+	public partial class SOSPage : ContentPage {
+		public SOSPage() {
+			InitializeComponent();
+		}
+
+		async void Handle_Clicked(object sender, System.EventArgs e) {
+			var SmsTask = MessagingPlugin.SmsMessenger;
+
+			var position = await CrossGeolocator.Current.GetPositionAsync(timeoutMilliseconds: 10000);
+			String pos = "Fadderullan SOS knapp test \nhttp://maps.google.com/?q=" + position.Latitude + "," + position.Longitude;
+
+			if (SmsTask.CanSendSms)
+				SmsTask.SendSms("+4794438252", pos);
+		}
+	}
+}
